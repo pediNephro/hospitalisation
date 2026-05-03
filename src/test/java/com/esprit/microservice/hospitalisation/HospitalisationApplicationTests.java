f3276528f0d2444d43a0cc636baf757510878a63
alaadiden <alaa.diden@esprit.com>
2026-05-03 01:28:52 +0100

    hospitalisaionapplicationtest

--- src/test/java/com/esprit/microservice/hospitalisation/HospitalisationApplicationTests.java
+++ src/test/java/com/esprit/microservice/hospitalisation/HospitalisationApplicationTests.java
@@ -2,12 +2,9 @@ package com.esprit.microservice.hospitalisation;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Disabled;
 
+@Disabled
 @SpringBootTest
 class HospitalisationApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
